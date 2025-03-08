104e29125fbddfae32aac7e5d37672d631f550b3
gitName <bartk@list.ru>
2025-03-08 21:50:21 +0300

    chore: implement database connection using PDO library

--- server/db/connection.php
+++ server/db/connection.php
@@ -2,3 +2,16 @@
 // access the environment variables
 require_once __DIR__ . '/../../vendor/autoload.php';
 (new App\Controller\DotEnvEnvironment)->load(__DIR__ . '/../../');
+
+// connect to the database
+try {
+    $host = $_ENV['HOST'];
+    $db = $_ENV['DATABASE'];
+    $user = $_ENV['USER'];
+    $password = $_ENV['PASSWORD'];
+    
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo json_encode(["Connection error: " => $e->getMessage()]);
+}
