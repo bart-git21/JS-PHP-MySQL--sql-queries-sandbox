377edaab9193ca22ad6bc7ce2ae03ad547f9a2d7
gitName <bartk@list.ru>
2025-03-08 21:52:14 +0300

    test: implement test query

--- server/index.php
+++ server/index.php
@@ -1,2 +1,9 @@
 <?php
 require_once __DIR__ . '/db/connection.php';
+
+$stmt = $conn->prepare("SELECT * FROM users");
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+header("Content-Type: application/json;charset=UTF-8");
+echo json_encode($result);
