037026921339eab4fa908a63f3574eabad63e162
gitName <bartk@list.ru>
2025-03-09 21:57:25 +0300

    refactor: handle the requests inside the switch statement

--- server/queries.php
+++ server/queries.php
@@ -1,11 +1,21 @@
 <?php
 include "db/connection.php";
 
-session_start();
-$userId = $_SESSION['userId'];
-$stmt = $conn->prepare("SELECT * FROM queries WHERE user_id = :userId");
-$stmt->bindParam(":userId", $userId);
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-header("Content-Type: application/json;charset=UTF-8");
-echo json_encode($result);
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+switch ($requestMethod) {
+    case "GET":
+        session_start();
+        $userId = $_SESSION['userId'];
+        $stmt = $conn->prepare("SELECT * FROM queries WHERE user_id = :userId");
+        $stmt->bindParam(":userId", $userId);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        header("Content-Type: application/json;charset=UTF-8");
+        echo json_encode($result);
+        break;
+    case "POST":
+        break;
+    default:
+        break;
+}
