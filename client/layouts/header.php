dcd1d8911c8020de08f0bc01b4e65bab810efeb2
gitName <bartk@list.ru>
2025-03-09 15:47:15 +0300

    feat: set an active class to the current page

--- client/layouts/header.php
+++ client/layouts/header.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-sm-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/client/index.php">Home</a>
+          <a class="nav-link" href="/client/index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/client/pages/query.php">Queries testing</a>
@@ -16,3 +16,19 @@
     </div>
   </div>
 </nav>
+
+<script>
+    $(".navbar").on("click", function(event) {
+        event.preventDefault();
+        if (event.target.classList.contains("nav-link")) {
+            console.log("nav-link");
+            const links = document.querySelectorAll(".nav-link");
+            links.forEach(e => {
+                e.classList.remove("active");
+                e.removeAttribute("aria-current");
+            })
+            event.target.classList.add("active");
+            event.target.setAttribute("aria-current", "page");
+        }
+    })
+</script>
