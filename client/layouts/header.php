89766c25fb76a94b93d5f969813024be3f78d49e
gitName <bartk@list.ru>
2025-03-09 17:53:43 +0300

    style: use prettier in the header

--- client/layouts/header.php
+++ client/layouts/header.php
@@ -1,20 +1,22 @@
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <div class="container-fluid">
-    <span class="navbar-brand mb-0 h1"><< Query sandbox >></span>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <span class="navbar-brand mb-0 h1">
+        Query sandbox
+    </span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/client/index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/client/pages/query.php">Queries testing</a>
-        </li>
-      </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+            <li class="nav-item">
+                <a class="nav-link" href="/client/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/client/pages/query.php">Queries
+                    testing</a>
+            </li>
+        </ul>
     </div>
-  </div>
 </nav>
 
 <script>
@@ -23,7 +25,7 @@
     (id + 1) && links[id].classList.add("active");
     (id + 1) && links[id].setAttribute("aria-current", "page");
 
-    $(".navbar").on("click", function(event) {
+    $(".navbar").on("click", function (event) {
         if (event.target.classList.contains("nav-link")) {
             links.forEach((e, i) => {
                 (e === event.target) && localStorage.setItem("activePage", i);
@@ -39,10 +41,12 @@
         border-bottom: 1px solid transparent;
         transition: all 0.5s;
     }
+
     .nav-item:hover {
         border-bottom: 1px solid black;
         background-color: #aaa;
     }
+
     .nav-item:has(a.active) {
         border-bottom: 1px solid black;
     }
