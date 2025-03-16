12ead61c5e34e4f90c6cc18ca56d019d2cefa15a
gitName <bartk@list.ru>
2025-03-16 18:47:56 +0300

    refactor: users.php, queries.php; merge all queries request and the logged user queries request into a single endpoint

--- server/queries.php
+++ server/queries.php
@@ -7,8 +7,12 @@ switch ($requestMethod) {
     case "GET":
         session_start();
         $userId = $_SESSION['userId'];
-        $stmt = $conn->prepare("SELECT * FROM queries WHERE user_id = :userId");
-        $stmt->bindParam(":userId", $userId);
+        if ($userId === "1") {
+            $stmt = $conn->prepare('SELECT q.name, q.query, q.user_id, users.login FROM queries AS q LEFT JOIN users ON q.user_id = users.id ORDER BY users.login ASC');
+        } else {
+            $stmt = $conn->prepare("SELECT * FROM queries WHERE user_id = :userId");
+            $stmt->bindParam(":userId", $userId);
+        }
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         header("Content-Type: application/json;charset=UTF-8");
