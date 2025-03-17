2319f67748ee3737745ebe88a5a70aeace68a237
gitName <bartk@list.ru>
2025-03-17 22:15:26 +0300

    refactor: use bootstrap varying modal content for login/registration modal

--- client/components/user_select.php
+++ client/components/user_select.php
@@ -1,9 +1,11 @@
 <div id="user" class="mr-2"></div>
 <!-- Button trigger modal -->
-<div id="loginBtn" type="button" class="btn btn-info btn-sm mr-2" data-toggle="modal" data-target="#usersModal">
+<div id="loginBtn" type="button" class="btn btn-info btn-sm mr-2" data-toggle="modal" data-target="#usersModal"
+    data-title="Sign in">
     Sign in
 </div>
-<div id="signOnBtn" type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#usersModal">
+<div id="signOnBtn" type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#usersModal"
+    data-title="Registration">
     Registration
 </div>
 
@@ -13,24 +15,27 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="usersModalLabel">Sign in</h5>
+                <h5 class="modal-title" id="usersModalLabel"></h5>
                 <button type="button" class="close btn-sm" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form id="loginForm">
-                    <label for="userLoginSelect">Login:</label>
-                    <select class="custom-select" id="userLoginSelect">
-                        <option selected disabled value="-1">Пользователь</option>
-                    </select>
+                    <div id="userLogin" class="form-group">
+                        <label for="userLoginSelect">Login:</label>
+                        <select class="custom-select" id="userLoginSelect">
+                            <option selected disabled value="-1">Пользователь</option>
+                        </select>
+                        <input id="userLoginInput" class="form-control" style="display:none;" type="text" placeholder="Enter the login">
+                    </div>
                     <div class="form-group">
                         <label for="userPassword">Password:</label>
                         <input type="password" class="form-control" id="userPassword" aria-describedby="emailHelp"
                             placeholder="Enter password">
                         <small id="emailHelp" class="form-text text-muted">Enter password.</small>
                     </div>
-                    <button id="submitBtn" class="btn btn-primary" type="submit">Sign in</button>
+                    <button id="submitBtn" class="btn btn-primary" type="submit"></button>
                 </form>
             </div>
         </div>
@@ -74,9 +79,24 @@
         const user = localStorage.getItem('user') || "";
         $("#user").text(user);
 
+        $('#usersModal').on('show.bs.modal', function (event) {
+            const button = $(event.relatedTarget); // Button that triggered the modal
+            const recipient = button.data('title'); // Extract info from data-* attributes
+            if (recipient === "Registration") {
+                $("#userLoginSelect").hide();
+                $("#userLoginInput").show();
+            } else {
+                $("#userLoginSelect").show();
+                $("#userLoginInput").hide();
+            }
+            const modal = $(this);
+            modal.find('.modal-title').text(recipient);
+            modal.find('#submitBtn').text(recipient);
+        })
+
         $("#signOnBtn").on("click", function () {
             $.ajax({
-                url: "/api/user/",
+                url: "/projects/php/php _ sql queries store/api/user/",
                 method: "POST",
                 data: {
                     login: $("#userLogin").val(),
@@ -100,7 +120,7 @@
         // create select option
         $("#loginBtn").on("click", function () {
             $.ajax({
-                url: "/api/login/",
+                url: "/projects/php/php _ sql queries store/api/login/",
                 method: "GET",
             })
                 .done(users => {
@@ -121,7 +141,7 @@
         $("#loginForm").on("submit", function (event) {
             event.preventDefault();
             $.ajax({
-                url: "/api/login/",
+                url: "/projects/php/php _ sql queries store/api/login/",
                 method: "POST",
                 data: JSON.stringify({
                     id: $("#userLoginSelect").val(),
